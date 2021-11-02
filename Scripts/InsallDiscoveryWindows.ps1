5d7eeb52d6c892e48aac8c8a97013e170f30dbec
VRIAMON1 <v.riamon@ynov.com>
2021-11-02 12:26:53 +0100

    Install discovery Windows

---
+++ Scripts/InsallDiscoveryWindows.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+chocolatey.exe feature enable -n allowGlobalConfirmation
+choco install git
+choco install nodejs
+choco install vscode
+refreshenv
